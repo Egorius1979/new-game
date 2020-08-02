79d4ea7b0cb37c959a6b86e623bb4de6400a4f2b
Egorius1979 <Egoriyvelikolepny1979@gmail.com>
2020-08-02 22:54:35 +0200

    fix tailwind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./tailwind.output.css";
 import Home from "./components/home";
 import store from "./app/store";
 import { Provider } from "react-redux";
